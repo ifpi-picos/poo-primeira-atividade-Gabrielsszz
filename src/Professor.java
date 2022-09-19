5824b7eda7b70d75d1e8ccc6273524ac5f2b1bda
Your Name <you@example.com>
2022-09-19 20:58:34 -0300

    second commit

--- src/Professor.java
+++ src/Professor.java
@@ -1,5 +1,25 @@
 public class Professor {
-        String nome;
-        String email;
-        String disciplina;
+        private String nome;
+        private String email;
+        private String disciplina;
+    
+    public Professor(String nome, String email, String disciplina){
+        this.nome = nome;
+        this.email = email;
+        this.disciplina = disciplina;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    
     }
